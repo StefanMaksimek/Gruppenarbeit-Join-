fbe3fbda958f650797e343194d47a85909156edc
Stefan Maksimek <stefan.maksimek@gmail.com>
2022-07-02 18:58:11 +0200

    add main frame

--- script.js
+++ script.js
@@ -93,3 +93,34 @@ function createTask() {
     tasks.push(task)
 
 }
+
+function openAddTask() {
+    closeAllContent()
+    document.getElementById('add-task').className = 'add-task'
+    document.getElementById('add-task-h3').className = 'active-h3'
+}
+
+function openImpressum() {
+    closeAllContent()
+    document.getElementById('impressum').className = 'impressum'
+    document.getElementById('impressum-h3').className = 'active-h3'
+}
+
+function openDatenschutz() {
+    closeAllContent()
+    document.getElementById('datenschutz').className = 'datenschutz'
+    document.getElementById('datenschutz-h3').className = 'active-h3'
+}
+
+
+function closeAllContent() {
+    document.getElementById('impressum').className = 'd-none'
+    document.getElementById('datenschutz').className = 'd-none'
+    document.getElementById('add-task').className = 'd-none'
+    document.getElementById('board-h3').className = ''
+    document.getElementById('backlog-h3').className = ''
+    document.getElementById('add-task-h3').className = ''
+    document.getElementById('help-h3').className = ''
+    document.getElementById('impressum-h3').className = ''
+    document.getElementById('datenschutz-h3').className = ''
+}
