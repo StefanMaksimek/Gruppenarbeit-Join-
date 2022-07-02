e24739e1bdb2f1700880817c64b81d053588a372
Michael Strauß <strauss.michael@gmx.net>
2022-07-02 05:48:58 +0200

    detailed add task

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+//Wir brauchen für den Bereich "editor" bei Add Task  ein Array mit allen Teilnehmern, die Zugang zu der Liste haben. Überschneidung mit Login
+let editors = ['Martin', 'Stefan', 'Julia', 'Michael', 'Sahra'];
+
+
+//Der aktuelle Bearbeiter wird über den Login definiert
+let currentEditor = '';
