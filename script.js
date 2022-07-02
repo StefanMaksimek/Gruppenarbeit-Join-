779bf0c0cb4eec83d99fa89fa8d187958d5371c3
Michael Strauß <strauss.michael@gmx.net>
2022-07-02 06:23:30 +0200

    change editor options in login and add task

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ let editors = ['Martin', 'Stefan', 'Julia', 'Michael', 'Sahra'];
 
 
 //Der aktuelle Bearbeiter wird über den Login definiert
-let currentEditor = '';
+let currentEditor;
+
+
+
+// Login Funktionen
+
+function loadEditors() {
+
+    let editorOptions = document.getElementById('all-editors');
+    let taskEditors = document.getElementById('task-editor');
+
+    for (let i = 0; i < editors.length; i++) {
+        const name = editors[i];
+        editorOptions.innerHTML += `<option value="${name}" onclick="setEditor(${name})"></option>`;
+        taskEditors.innerHTML += `<option value="${name}" onclick="setEditor(${name})"></option>`;
+    }
+}
+
+function setEditor(name){
+    currentEditor = `${name}`;
+}
+
+console.log(currentEditor)
