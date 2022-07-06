77be3c113a4d774f19bcef259caaac45c8f2e9eb
Michael Strauß <strauss.michael@gmx.net>
2022-07-06 07:01:15 +0200

    add task color selection

--- script.js
+++ script.js
@@ -8,10 +8,23 @@ let tasks = [
         "status": 'ToDo',
         "description": 'create some ideas how we can resolve the problem of the responsible design and the arrangement of the object of the side'
     }
-]
+];
+
+let temporaryArrayColor = [];
 
 
 
+
+
+function showColor() {
+    let content = document.getElementById('color-picker')
+    content.classList.remove('d-none')
+}
+
+
+function takeColor(backgroundColor){
+    temporaryArrayColor.push(backgroundColor)
+}
 
 
 function createTask() {
@@ -22,6 +35,7 @@ function createTask() {
     let user = document.getElementById('task-user-input');
     let status = document.getElementById('status-list-input');
     let description = document.getElementById('task-description');
+    let color = temporaryArrayColor[0]
 
     let task = {
         "title": title.value,
@@ -30,7 +44,8 @@ function createTask() {
         "due-date": '',
         "user": user.value,
         "status": status.value,
-        "description": description.value
+        "description": description.value,
+        "color": color //background color
     }
 
     tasks.push(task)
