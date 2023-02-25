09a75212056375bd7d04ab8ebfa1a648c66e9b08
Michael Strauß <strauss.michael@gmx.net>
2023-02-25 19:35:05 +0100

    further coding change task in backlog detail view

--- scriptImporte/backlog.js
+++ scriptImporte/backlog.js
@@ -49,6 +49,76 @@ function closeBacklogDetails() {
 }
 
 
+function changeTaskBacklog(id) {
+    let currentTask = tasks.find(t => t.id == id);
+    //assigned user
+    //setValuesForChangeTaskBacklogAssignedUser(currentTask)
+    //title
+    setValuesForChangeTaskBacklogTitle(currentTask);
+    //category
+    setValuesForChangeTaskBacklogCategory(currentTask);
+    //due date
+    setValuesForChangeTaskBacklogDueDate(currentTask);
+    //priority
+    setValuesForChangeTaskBacklogPriority(currentTask);
+    //description
+    setValuesForChangeTaskBacklogDescription(currentTask);
+    //buttons
+    //document.getElementById('show-board-details-box-btns').innerHTML = changeButtonsforEditorModusOnBoardChangeTask(id);
+}
+
+
+function setValuesForChangeTaskBacklogDueDate(currentTask) {
+    document.getElementById('show-backlog-details-box-due-date').classList.add('d-none')
+    document.getElementById('due-date-backlog-details-box').classList.remove('d-none')
+    document.getElementById('due-date-backlog-details-box').value = transformTimeStampToDate(currentTask.dueDate);
+}
+
+
+function setValuesForChangeTaskBacklogCategory(currentTask) {
+    document.getElementById('show-backlog-details-box-category').classList.add('d-none')
+    document.getElementById('category-state-input-change-detail-box-backlog').classList.remove('d-none')
+    if (currentTask.category == 'IT') {
+        document.getElementById('category-state-input-backlog-details-box-option-it').selected = 'selected'
+    }
+    if (currentTask.category == 'Marketing') {
+        document.getElementById('category-state-input-backlog-details-box-option-marketing').selected = 'selected'
+    }
+    if (currentTask.category == 'Sales') {
+        document.getElementById('category-state-input-backlog-details-box-option-sales').selected = 'selected'
+    }
+    if (currentTask.category == 'Management') {
+        document.getElementById('category-state-input-backlog-details-box-option-management').selected = 'selected'
+    }
+}
+
+
+function setValuesForChangeTaskBacklogTitle(currentTask) {
+    document.getElementById('show-backlog-details-box-title').classList.add('d-none')
+    document.getElementById('title-task-change-detail-box-backlog').classList.remove('d-none')
+    document.getElementById('title-task-change-detail-input-backlog').value = currentTask.title
+}
+
+
+function setValuesForChangeTaskBacklogPriority(currentTask) {
+    document.getElementById('show-backlog-details-box-priority').classList.add('d-none')
+    document.getElementById('show-backlog-details-box-priority-change').classList.remove('d-none')
+    if (currentTask.priority == 'normal') {
+        document.getElementById('priority-state-input-backlog-details-box-option-normal').selected = 'selected'
+    }
+    else {
+        document.getElementById('priority-state-input-backlog-details-box-option-high').selected = 'selected'
+    }
+}
+
+
+function setValuesForChangeTaskBacklogDescription(currentTask) {
+    document.getElementById('show-backlog-details-box-details').classList.add('d-none')
+    document.getElementById('task-description-backlog-details-box').classList.remove('d-none')
+    document.getElementById('task-description-backlog-details-input').value = currentTask.description
+}
+
+
 function deleteTask(id) {
     let currentTask = tasks.find(t => t.id == id);
     let index = tasks.indexOf(currentTask);
