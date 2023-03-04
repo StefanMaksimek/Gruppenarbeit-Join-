508627df44f0c08f337c88911434f5589c6211c5
Michael Strauß <strauss.michael@gmx.net>
2023-03-04 07:04:34 +0100

    summary welcome to user

--- scriptImporte/summary.js
+++ scriptImporte/summary.js
@@ -6,16 +6,32 @@ function loadSummaryContent() {
             tasksOfUser.push(task)
         }
     })
-    document.getElementById('summary-nbr-tasks-board').innerHTML = tasksOfUser.filter(t => t.locationTask == 'board').length;
-    document.getElementById('summary-nbr-tasks-backlog').innerHTML = tasksOfUser.filter(t => t.locationTask == 'backlog').length;
-    document.getElementById('summary-nbr-tasks-feedback').innerHTML = tasksOfUser.filter(t => t.status == 'done').length;
-    document.getElementById('summary-nbr-tasks-progress').innerHTML = tasksOfUser.filter(t => t.status == 'in-progress').length;
-    document.getElementById('summary-to-do-nbr').innerHTML = tasksOfUser.filter(t => t.status == 'to-do' && t.locationTask == 'board').length;
-    document.getElementById('summary-done-nbr').innerHTML = tasksOfUser.filter(t => t.status == 'done' && t.locationTask == 'board').length;
-    let urgentsTasks = findUrgentTasks(tasksOfUser);
-    document.getElementById('summary-urgent-nbr').innerHTML = urgentsTasks.length;
+    if (tasksOfUser.length > 0) {
+        document.getElementById('summary-nbr-tasks-board').innerHTML = tasksOfUser.filter(t => t.locationTask == 'board').length;
+        document.getElementById('summary-nbr-tasks-backlog').innerHTML = tasksOfUser.filter(t => t.locationTask == 'backlog').length;
+        document.getElementById('summary-nbr-tasks-feedback').innerHTML = tasksOfUser.filter(t => t.status == 'testing').length;
+        document.getElementById('summary-nbr-tasks-progress').innerHTML = tasksOfUser.filter(t => t.status == 'in-progress').length;
+        document.getElementById('summary-to-do-nbr').innerHTML = tasksOfUser.filter(t => t.status == 'to-do' && t.locationTask == 'board').length;
+        document.getElementById('summary-done-nbr').innerHTML = tasksOfUser.filter(t => t.status == 'done' && t.locationTask == 'board').length;
+        let urgentsTasks = findUrgentTasks(tasksOfUser);
+        document.getElementById('summary-urgent-nbr').innerHTML = urgentsTasks.length;
+    }
+    else {
+        fillSummaryCardsWithZero()
+    }
+    document.getElementById('summary-name').innerHTML = returnUsersName();
 }
 
+
+function fillSummaryCardsWithZero(){
+    document.getElementById('summary-nbr-tasks-board').innerHTML = 0;
+    document.getElementById('summary-nbr-tasks-backlog').innerHTML = 0;
+    document.getElementById('summary-nbr-tasks-feedback').innerHTML = 0;
+    document.getElementById('summary-nbr-tasks-progress').innerHTML = 0;
+    document.getElementById('summary-to-do-nbr').innerHTML = 0;
+    document.getElementById('summary-done-nbr').innerHTML = 0;
+    document.getElementById('summary-urgent-nbr').innerHTML = 0;
+}
 
 //find tasks with deadline behind or in 10 days
 function findUrgentTasks(tasksOfUser) {
@@ -30,5 +46,13 @@ function findUrgentTasks(tasksOfUser) {
 function getUrgentTasksNextDate(urgentTasks) {
     let newSortedUrgentTasks = urgentTasks.sort(function (a, b) { return a.dueDate - b.dueDate })
     document.getElementById('summary-next-due-date').innerHTML = new Date(newSortedUrgentTasks[0].dueDate).toISOString().substring(0, 10);
+}
+
+
+function returnUsersName() {
+    const usersMail = localStorage.getItem('joinLoginMail');
+    let user = users.find(u => u.mail == usersMail)
+    console.log(user)
+    return user.name
 }
 
